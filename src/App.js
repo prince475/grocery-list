9d09481989bb9c63d236f729303dfd7142b5b95f
prince475 <danielmwai475@gmail.com>
2023-05-11 22:50:41 +0300

    Creating a fetch function and catching errors that may occur.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 
 function App() {
-	const [items, setItems] = useState(
-		JSON.parse(localStorage.getItem('shoppingList'))
-	);
+	const API_URL = 'http://localhost:3500/items';
 
+	const [items, setItems] = useState([]);
 	const [newItem, setNewItem] = useState('');
 	const [search, setSearch] = useState('');
+	const [fetchError, setFetchError] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
 
-	const setAndSaveItems = (newItems) => {
-		setItems(newItems);
-		localStorage.setItem('shoppingList', JSON.stringify(newItems));
-	};
+	useEffect(() => {
+
+		const fetchItems = async () => {
+			try {
+				const response = await fetch(API_URL);
+				if(!response.ok) throw Error('Did not receive expected data');
+				const listItems = await response.json();
+				setItems(listItems);
+				setFetchError(null);
+			} catch (err) {
+				setFetchError(err.message)
+
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		setTimeout(() => {
+		(async () => await fetchItems())();
+		}, 2000);
+
+	}, [])
+
 	const addItem = (item) => {
 		const id = items.length ? items[items.length - 1].id + 1 : 1;
 		const myNewItem = { id: id, checked: false, item };
 		const listItems = [...items, myNewItem];
-		setAndSaveItems(listItems);
+		setItems(listItems);
 	};
 
 	const handleSubmit = (e) => {
@@ -39,12 +58,12 @@ function App() {
 			item.id === id ? { ...item, checked: !item.checked } : item
 		);
 
-		setAndSaveItems(listItems);
+		setItems(listItems);
 	};
 
 	const handleDelete = (id) => {
 		const listItems = items.filter((item) => item.id !== id);
-		setAndSaveItems(listItems);
+		setItems(listItems);
 	};
 
 	return (
@@ -56,14 +75,18 @@ function App() {
 				setNewItem={setNewItem}
 				handleSubmit={handleSubmit}
 			/>
-			<Content
-				items={items.filter(item =>
-					item.item.toLowerCase().includes(search.toLowerCase())
-				)}
-				setItems={setItems}
-				handleCheck={handleCheck}
-				handleDelete={handleDelete}
-			/>
+			<main>
+				{isLoading && <p>Loading Items...</p>}
+				{fetchError && <p style={{ color: "red"}}>{ `Error: ${fetchError}`}</p>}
+				{!fetchError && !isLoading && <Content
+					items={items.filter(item =>
+						item.item.toLowerCase().includes(search.toLowerCase())
+					)}
+					setItems={setItems}
+					handleCheck={handleCheck}
+					handleDelete={handleDelete}
+				/>}
+			</main >
 			<Footer length={items.length} />
 		</div>
 	);
